3f68aa34b162ba5b972d81f3c1747fa8fca56f1d
Gerd Jungbluth <gerd.jungbluth@engawa.de>
2021-08-11 18:19:36 +0000

    feat(headline): add dynamic headline Fixes #5

---
+++ src/app/basic/model/topbarOptions.ts
@@ -0,0 +1,9 @@
+/**
+ * The configurable options for the witched top bar
+ */
+export interface TopbarOptions {
+  /**
+   * The headline of the topbar
+   */
+  headline?: string;
+}
--- src/app/basic/witched-headline/witched-headline.component.ts
+++ src/app/basic/witched-headline/witched-headline.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'be-witched-headline',
   templateUrl: './witched-headline.component.html',
-  styleUrls: ['./witched-headline.component.scss']
+  styleUrls: ['./witched-headline.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WitchedHeadlineComponent implements OnInit {
+  /**
+   * The headline to be displayed
+   */
+  @Input('beHeadline')
+  headline? = WitchedHeadlineComponent.name;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
