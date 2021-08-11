7d1dc3f0448541e288e532e7b080284505347760
Gerd Jungbluth <gerd.jungbluth@engawa.de>
2021-08-11 18:43:37 +0000

    feat(container): add options to configure container Fixes #7

---
+++ src/app/basic/model/container-options.ts
@@ -0,0 +1,11 @@
+import { TopbarOptions } from './topbar-options';
+
+/**
+ * The configurable options for the witched container
+ */
+export interface ContainerOptions {
+  /**
+   * The top bar options
+   */
+  topbar?: TopbarOptions;
+}
--- src/app/basic/witched-container/witched-container.component.ts
+++ src/app/basic/witched-container/witched-container.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ContainerOptions } from '../model/container-options';
 
 @Component({
   selector: 'be-witched-container',
   templateUrl: './witched-container.component.html',
-  styleUrls: ['./witched-container.component.scss']
+  styleUrls: ['./witched-container.component.scss'],
 })
 export class WitchedContainerComponent implements OnInit {
+  @Input('beContainerOptions')
+  options!: ContainerOptions;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
