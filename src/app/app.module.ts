44136be4041b57e27f7969f69dafdf94279fb1bf
Gerd Jungbluth <gerd.jungbluth@engawa.de>
2021-08-13 17:32:10 +0000

    feat(measurement): add measurement module Fixes #14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BasicModule } from './basic/basic.module';
+import { MeasurementModule } from './measurement/measurement.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BasicModule, PlatformModule],
+  imports: [
+    AppRoutingModule,
+    BasicModule,
+    BrowserModule,
+    MeasurementModule,
+    PlatformModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
